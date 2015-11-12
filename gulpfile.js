b06f543ec3c539e7f19ca46d458ffb87f0f069c5
liuxiong <liuxiong332@163.com>
2015-11-12 17:17:57 +0800

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+
+function compile(watch) {
+  var bundler = watchify(
+    browserify('./app/main.js', {debug: true})
+    .transform("babelify", {presets: ["es2015", "react"]})
+  );
+
+  function rebundle() {
+    return bundler.bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      // optional, remove if you dont want sourcemaps
+      .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+       // Add transformation tasks to the pipeline here.
+      .pipe(sourcemaps.write('./build')) // writes .map file
+      .pipe(gulp.dest('./build/js/'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  return rebundle();
+}
+
+gulp.task('sass', function () {
+  gulp.src('./styles/app.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/style'));
+});
+
+gulp.task('watch-sass', function() {
+  gulp.watch('./styles/*.scss', ['sass']);
+});
+
+//start local dev svr
+gulp.task('connect', function() {
+  connect.server({
+    root: ['./build'],
+    livereload: true
+  });
+});
+
+gulp.task('js', function() { return compile(); });
+gulp.task('watch', function() { return compile(true); });
+gulp.task('build', ['js', 'sass']);
+gulp.task('default', ['watch', 'watch-sass', 'connect']);
